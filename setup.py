11c53acbd3c9d92326bc31eced8c20b30748187a
Minwell <chenyimin@gmail.com>
2020-12-21 19:39:17 +0800

    FIx 1.7 setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import os
 from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
-from os.path import join
 
-CPU_ONLY = False
+import torch
+from torch.utils.cpp_extension import CUDA_HOME, CUDAExtension, CppExtension
+
 project_root = 'Correlation_Module'
+
+torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+assert torch_ver >= [1, 5], "Requires PyTorch >= 1.5"
 
 source_files = ['correlation.cpp', 'correlation_sampler.cpp']
 
@@ -11,16 +15,52 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-def launch_setup():
-    if CPU_ONLY:
-        Extension = CppExtension
-        macro = []
-    else:
-        Extension = CUDAExtension
-        source_files.append('correlation_cuda_kernel.cu')
-        macro = [("USE_CUDA", None)]
+def get_extension():
+    from torch.utils.cpp_extension import ROCM_HOME
+    is_rocm_pytorch = (
+        True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
+    )
+    extension = CppExtension
 
-    sources = [join(project_root, file) for file in source_files]
+    extra_compile_args = {"cxx": []}
+    define_macros = []
+
+    if (torch.cuda.is_available() and ((CUDA_HOME is not None) or is_rocm_pytorch)) or os.getenv(
+        "FORCE_CUDA", "0"
+    ) == "1":
+        extension = CUDAExtension
+        if not is_rocm_pytorch:
+            define_macros += [("WITH_CUDA", None)]
+            extra_compile_args["nvcc"] = [
+                "-O3",
+                "-DCUDA_HAS_FP16=1",
+                "-D__CUDA_NO_HALF_OPERATORS__",
+                "-D__CUDA_NO_HALF_CONVERSIONS__",
+                "-D__CUDA_NO_HALF2_OPERATORS__",
+            ]
+        else:
+            define_macros += [("WITH_HIP", None)]
+            extra_compile_args["nvcc"] = []
+
+        if torch_ver < [1, 7]:
+            # supported by https://github.com/pytorch/pytorch/pull/43931
+            CC = os.environ.get("CC", None)
+            if CC is not None:
+                extra_compile_args["nvcc"].append("-ccbin={}".format(CC))
+
+    sources = [os.path.join(project_root, file) for file in source_files]
+    ext_modules = [
+        extension(
+            'spatial_correlation_sampler_backend',
+            sources,
+            define_macros=define_macros,
+            extra_compile_args=extra_compile_args,
+            extra_link_args=['-lgomp'])
+        ]
+    return ext_modules
+
+
+def launch_setup():
 
     setup(
         name='spatial_correlation_sampler',
@@ -32,18 +72,10 @@ def launch_setup():
         long_description_content_type="text/markdown",
         url="https://github.com/ClementPinard/Pytorch-Correlation-extension",
         install_requires=['torch>=1.1', 'numpy'],
-        ext_modules=[
-            Extension('spatial_correlation_sampler_backend',
-                      sources,
-                      define_macros=macro,
-                      extra_compile_args={'cxx': ['-fopenmp'], 'nvcc':[]},
-                      extra_link_args=['-lgomp'])
-        ],
+        ext_modules=get_extension(),
         package_dir={'': project_root},
         packages=['spatial_correlation_sampler'],
-        cmdclass={
-            'build_ext': BuildExtension
-        },
+        cmdclass={'build_ext': torch.utils.cpp_extension.BuildExtension},
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
